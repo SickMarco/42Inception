3ef2db81b97be9fc51bd4b26736d65cc65ead51a
SickMarco <marcobo97@hotmail.it>
2023-08-21 19:00:32 +0200

    wordpress + db

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir -p /run/mysqld
+mkdir -p /run/mysqld /var/log/mysql
 
 if [ ! -d /var/lib/mysql/$MYSQL_DATABASE ]; then
 
@@ -10,14 +10,13 @@ cat << EOF > init.sql
 USE mysql;
 FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
-CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
 EOF
 
 mysqld --user=root --bootstrap < init.sql
-
 rm -f init.sql
 fi
 
-mysqld --user=root
+exec /usr/bin/mysqld_safe --defaults-file=/etc/mysql/my.cnf
