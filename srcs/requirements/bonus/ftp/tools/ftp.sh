d2124e83beeb5ccd4175611909c78baaa7ffde8d
SickMarco <marcobo97@hotmail.it>
2023-08-26 18:20:11 +0200

    Major fix && ftp

--- srcs/requirements/bonus/ftp/tools/ftp.sh
+++ srcs/requirements/bonus/ftp/tools/ftp.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 if ! id "$FTP_USER" >/dev/null 2>&1; then
-adduser -D -h /home/$FTP_USER -s /sbin/nologin $FTP_USER && echo "$FTP_USER:$FTP_USER_PASS" | chpasswd
-mkdir -p /home/$FTP_USER/ftp
-chown -R $FTP_USER:$FTP_USER /home/$FTP_USER/ftp /ftp
+adduser -D -h /ftp -s /sbin/nologin $FTP_USER && echo "$FTP_USER:$FTP_USER_PASS" | chpasswd
+chown -R $FTP_USER:$FTP_USER /ftp
 fi
 
 vsftpd /etc/vsftpd/vsftpd.conf
