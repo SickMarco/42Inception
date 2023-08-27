012c483357fc8e0f2a304e405935c2d297b3aa5e
SickMarco <marcobo97@hotmail.it>
2023-08-27 20:25:12 +0200

    Fixes

--- srcs/requirements/bonus/ftp/tools/ftp.sh
+++ srcs/requirements/bonus/ftp/tools/ftp.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 if ! id "$FTP_USER" >/dev/null 2>&1; then
+addgroup wpgroup
 adduser -D -h /ftp -s /sbin/nologin $FTP_USER && echo "$FTP_USER:$FTP_USER_PASS" | chpasswd
-chown -R $FTP_USER:$FTP_USER /ftp
+addgroup $FTP_USER wpgroup
 fi
 
 vsftpd /etc/vsftpd/vsftpd.conf
--- srcs/requirements/wordpress/tools/wp_init.sh
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -2,44 +2,26 @@
 
 if [ ! -d /var/www/html/wp-admin ]; then
 
+wget https://wordpress.org/latest.tar.gz
+tar -xzvf latest.tar.gz -C /var/www/html --strip-components=1 > /dev/null
+rm latest.tar.gz
+
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
+
+envsubst '${MYSQL_DATABASE} ${MYSQL_USER} ${MYSQL_PASSWORD}' < wp-config.php > /var/www/html/wp-config.php
+
 cd /var/www/html
-
-wp core download
-
-cat << EOF > wp-config.php
-<?php
-
-define('DB_NAME', '$MYSQL_DATABASE');
-define('DB_USER', '$MYSQL_USER');
-define('DB_PASSWORD', '$MYSQL_PASSWORD');
-define('DB_HOST', 'db');
-define('DB_CHARSET', 'utf8');
-define('DB_COLLATE', 'utf8_general_ci');
-define( 'WP_DEBUG', true );
-define('WP_ALLOW_REPAIR', true);
-define('WP_CACHE', true);
-define('WP_REDIS_HOST', 'rds');
-define('WP_REDIS_PORT', '6379');
-EOF
-
-echo '$table_prefix' = "'wp_';" >> wp-config.php
-
-cat << EOF >> wp-config.php
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
-}
-require_once ABSPATH . 'wp-settings.php';
-?>
-EOF
-
 wp core install --url=https://localhost --title="${WP_TITLE}" --admin_user="${WP_ADMIN}" --admin_password="${WP_PASS}" --admin_email="${WP_EMAIL}"
 wp plugin install redis-cache --activate
 wp redis enable
+
+addgroup wpgroup
+chown -R :wpgroup /var/www/html
 fi
 
+rm /wp-config.php
 mkdir -p /var/log/php-fpm/ /run/php-fpm/
 sed -i 's/127.0.0.1:9000/0.0.0.0:9000/g' /etc/php81/php-fpm.d/www.conf
 php-fpm81 --nodaemonize
