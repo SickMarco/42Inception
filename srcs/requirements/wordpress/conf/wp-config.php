06c39d54dcac68ae65f3ed68819912f682561d63
SickMarco <marcobo97@hotmail.it>
2023-08-28 16:47:45 +0200

    Wp auto-install v2

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,16 +1,16 @@
 <?php
 
-define('DB_NAME', '$MYSQL_DATABASE');
-define('DB_USER', '$MYSQL_USER');
-define('DB_PASSWORD', '$MYSQL_PASSWORD');
-define('DB_HOST', 'db');
-define('DB_CHARSET', 'utf8');
-define('DB_COLLATE', 'utf8_general_ci');
+define( 'DB_NAME', '$MYSQL_DATABASE' );
+define( 'DB_USER', '$MYSQL_USER' );
+define( 'DB_PASSWORD', '$MYSQL_PASSWORD' );
+define( 'DB_HOST', 'db' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', 'utf8_general_ci' );
 define( 'WP_DEBUG', true );
-define('WP_ALLOW_REPAIR', true);
-define('WP_CACHE', true);
-define('WP_REDIS_HOST', 'rds');
-define('WP_REDIS_PORT', '6379');
+define( 'WP_ALLOW_REPAIR', true );
+define( 'WP_CACHE', true );
+define( 'WP_REDIS_HOST', 'rds' );
+define( 'WP_REDIS_PORT', '6379' );
 
 $table_prefix = 'wp_';
 
--- srcs/requirements/wordpress/tools/wp_init.sh
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -2,17 +2,9 @@
 
 if [ ! -d /var/www/html/wp-admin ]; then
 
-wget https://wordpress.org/latest.tar.gz
-tar -xzvf latest.tar.gz -C /var/www/html --strip-components=1 > /dev/null
-rm latest.tar.gz
-
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-
 envsubst '${MYSQL_DATABASE} ${MYSQL_USER} ${MYSQL_PASSWORD}' < wp-config.php > /var/www/html/wp-config.php
-
 cd /var/www/html
+wp core download
 wp core install --url=https://localhost --title="${WP_TITLE}" --admin_user="${WP_ADMIN}" --admin_password="${WP_PASS}" --admin_email="${WP_EMAIL}"
 wp plugin install redis-cache --activate
 wp redis enable
@@ -21,7 +13,7 @@ addgroup wpgroup
 chown -R :wpgroup /var/www/html
 fi
 
-rm /wp-config.php
+rm -f /wp-config.php
 mkdir -p /var/log/php-fpm/ /run/php-fpm/
 sed -i 's/127.0.0.1:9000/0.0.0.0:9000/g' /etc/php81/php-fpm.d/www.conf
 php-fpm81 --nodaemonize
